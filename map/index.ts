d2eaaebb8285c5aaa1a42a1fbc97e18e537784a8
siliconspecter <siliconspecter@outlook.com>
2022-01-03 16:43:30 +0000

    Implement mapper.

---
+++ map/index.ts
@@ -0,0 +1,419 @@
+import { resume, start } from "@skitscript/interpreter-nodejs";
+import type {
+  InterpreterState,
+  InterpreterStateCharacter,
+  Map,
+  MapStateCharacter,
+  MapStateInteraction,
+  MapStateRun,
+  ValidDocument,
+  ValidInterpreterState,
+} from "@skitscript/types-nodejs";
+import objectHash = require("object-hash");
+
+type RecursedDismissInterpreterState = {
+  readonly type: `dismiss`;
+  readonly interpreterState: ValidInterpreterState;
+  next?: RecursedInterpreterState;
+};
+
+type RecursedMenuInterpreterState = {
+  readonly type: `menu`;
+  readonly interpreterState: ValidInterpreterState;
+  readonly options: RecursedInterpreterState[];
+};
+
+type RecursedInterpreterState =
+  | RecursedDismissInterpreterState
+  | RecursedMenuInterpreterState;
+
+type StateAppearance = {
+  readonly characters: ReadonlyArray<MapStateCharacter>;
+  readonly speakers: ReadonlyArray<string>;
+  readonly background: null | string;
+  readonly line: null | ReadonlyArray<MapStateRun>;
+  readonly options: null | ReadonlyArray<ReadonlyArray<MapStateRun>>;
+};
+
+type StateAppearanceRecord = {
+  readonly stateAppearance: StateAppearance;
+  readonly recursedInterpreterStates: RecursedInterpreterState[];
+};
+
+const generateMapStateCharacter = (
+  interpreterStateCharacter: InterpreterStateCharacter
+): MapStateCharacter => {
+  switch (interpreterStateCharacter.state.type) {
+    case `notPresent`:
+      return { type: `notPresent` };
+
+    case `entering`:
+      return {
+        type: `entering`,
+        emote: interpreterStateCharacter.emote,
+        animation: interpreterStateCharacter.state.animation,
+      };
+
+    case `present`:
+      return { type: `present`, emote: interpreterStateCharacter.emote };
+
+    case `exiting`:
+      return {
+        type: `exiting`,
+        emote: interpreterStateCharacter.emote,
+        animation: interpreterStateCharacter.state.animation,
+      };
+  }
+};
+
+export const map = (document: ValidDocument): Map => {
+  // #region Crawl the entire state space of the game.
+  const recursedInterpreterStatesByInterpreterStateHashes: {
+    [interpreterStateHash: string]: RecursedInterpreterState;
+  } = {};
+
+  const recurse = (
+    interpreterState: InterpreterState
+  ): null | RecursedInterpreterState => {
+    switch (interpreterState.type) {
+      case `valid`:
+        {
+          const hash = objectHash(interpreterState);
+
+          if (
+            Object.prototype.hasOwnProperty.call(
+              recursedInterpreterStatesByInterpreterStateHashes,
+              hash
+            )
+          ) {
+            return recursedInterpreterStatesByInterpreterStateHashes[
+              hash
+            ] as RecursedMenuInterpreterState;
+          } else {
+            switch (interpreterState.interaction.type) {
+              case `dismiss`: {
+                const newRecord: RecursedDismissInterpreterState = {
+                  type: `dismiss`,
+                  interpreterState,
+                };
+
+                recursedInterpreterStatesByInterpreterStateHashes[hash] =
+                  newRecord;
+
+                const next = recurse(
+                  resume(
+                    document,
+                    interpreterState,
+                    interpreterState.interaction.instructionIndex
+                  )
+                );
+
+                if (next === null) {
+                  return null;
+                } else {
+                  newRecord.next = next;
+                  return newRecord;
+                }
+              }
+
+              case `menu`: {
+                const newRecord: RecursedMenuInterpreterState = {
+                  type: `menu`,
+                  interpreterState,
+                  options: [],
+                };
+
+                recursedInterpreterStatesByInterpreterStateHashes[hash] =
+                  newRecord;
+
+                for (const option of interpreterState.interaction.options) {
+                  const next = recurse(
+                    resume(document, interpreterState, option.instructionIndex)
+                  );
+
+                  if (next === null) {
+                    return null;
+                  } else {
+                    newRecord.options.push(next);
+                  }
+                }
+
+                return newRecord;
+              }
+            }
+          }
+        }
+        /* istanbul ignore next */
+        // @ts-expect-error Workaround for ESLint bug; ESLint is unaware that
+        //                  the above CAN'T fall through due to returns, TSC is
+        //                  aware and raises a warning in response.  Structuring
+        //                  the code like this ensures a TSC error if new
+        //                  interaction types are introduced.
+        break;
+
+      case `invalid`:
+        switch (interpreterState.error.type) {
+          case `infiniteLoop`:
+            return null;
+        }
+    }
+  };
+
+  const startingRecursedInterpreterState = recurse(start(document));
+  // #endregion
+
+  if (startingRecursedInterpreterState === null) {
+    return {
+      type: `invalid`,
+      error: {
+        type: `infiniteLoop`,
+      },
+    };
+  } else {
+    // #region Ensure that the starting state is first.
+    const recursedInterpreterStates = Object.values(
+      recursedInterpreterStatesByInterpreterStateHashes
+    );
+
+    recursedInterpreterStates.splice(
+      recursedInterpreterStates.indexOf(startingRecursedInterpreterState),
+      1
+    );
+
+    recursedInterpreterStates.unshift(startingRecursedInterpreterState);
+    // #endregion
+
+    // #region Group the crawled states by their appearances; identical-looking
+    //         states will be held together.
+    const stateAppearanceRecordsByStateApperanceHashes: {
+      [hash: string]: StateAppearanceRecord;
+    } = {};
+
+    for (const recursedInterpreterState of recursedInterpreterStates) {
+      const stateAppearance: StateAppearance = {
+        characters: recursedInterpreterState.interpreterState.characters.map(
+          (character) => generateMapStateCharacter(character)
+        ),
+        speakers: recursedInterpreterState.interpreterState.speakers,
+        background: recursedInterpreterState.interpreterState.background,
+        line:
+          recursedInterpreterState.interpreterState.line === null
+            ? null
+            : recursedInterpreterState.interpreterState.line.map((run) => ({
+                bold: run.bold,
+                italic: run.italic,
+                code: run.code,
+                plainText: run.plainText,
+              })),
+        options:
+          recursedInterpreterState.interpreterState.interaction.type ===
+          `dismiss`
+            ? null
+            : recursedInterpreterState.interpreterState.interaction.options.map(
+                (option) =>
+                  option.content.map((run) => ({
+                    bold: run.bold,
+                    italic: run.italic,
+                    code: run.code,
+                    plainText: run.plainText,
+                  }))
+              ),
+      };
+
+      const stateAppearanceHash = objectHash(stateAppearance);
+
+      if (
+        Object.prototype.hasOwnProperty.call(
+          stateAppearanceRecordsByStateApperanceHashes,
+          stateAppearanceHash
+        )
+      ) {
+        (
+          stateAppearanceRecordsByStateApperanceHashes[
+            stateAppearanceHash
+          ] as StateAppearanceRecord
+        ).recursedInterpreterStates.push(recursedInterpreterState);
+      } else {
+        stateAppearanceRecordsByStateApperanceHashes[stateAppearanceHash] = {
+          stateAppearance,
+          recursedInterpreterStates: [recursedInterpreterState],
+        };
+      }
+    }
+    // #endregion
+
+    // #region Ensure that the state appearance record with the starting
+    //         recursed interpreter state is first.
+    const stateAppearanceRecords = Object.values(
+      stateAppearanceRecordsByStateApperanceHashes
+    );
+
+    const indexOfStateAppearanceRecordContainingStartingRecursedInterpreterState =
+      stateAppearanceRecords.findIndex((stateAppearanceRecord) =>
+        stateAppearanceRecord.recursedInterpreterStates.includes(
+          startingRecursedInterpreterState
+        )
+      );
+    stateAppearanceRecords.unshift(
+      stateAppearanceRecords[
+        indexOfStateAppearanceRecordContainingStartingRecursedInterpreterState
+      ] as StateAppearanceRecord
+    );
+    stateAppearanceRecords.splice(
+      indexOfStateAppearanceRecordContainingStartingRecursedInterpreterState +
+        1,
+      1
+    );
+    // #endregion
+
+    // #region Merge visually identical states.  This is an iterative process;
+    //         it is possible that long parallel chains of identical states
+    //         exist, and each pass will merge at least one set of matching
+    //         "links".
+    let thisIterationMadeChanges = true;
+
+    while (thisIterationMadeChanges) {
+      thisIterationMadeChanges = false;
+
+      for (const stateAppearanceRecord of stateAppearanceRecords) {
+        for (
+          let i = 0;
+          i < stateAppearanceRecord.recursedInterpreterStates.length;
+          i++
+        ) {
+          const firstRecursedInterpreterState = stateAppearanceRecord
+            .recursedInterpreterStates[i] as RecursedInterpreterState;
+
+          for (
+            let j = i + 1;
+            j < stateAppearanceRecord.recursedInterpreterStates.length;
+
+          ) {
+            const secondRecursedInterpreterState = stateAppearanceRecord
+              .recursedInterpreterStates[j] as RecursedInterpreterState;
+
+            if (
+              (firstRecursedInterpreterState.type === `dismiss` &&
+                secondRecursedInterpreterState.type === `dismiss` &&
+                firstRecursedInterpreterState.next ===
+                  secondRecursedInterpreterState.next) ||
+              (firstRecursedInterpreterState.type === `menu` &&
+                secondRecursedInterpreterState.type === `menu` &&
+                firstRecursedInterpreterState.options.length ===
+                  secondRecursedInterpreterState.options.length &&
+                firstRecursedInterpreterState.options.every(
+                  (firstOption, optionIndex) =>
+                    firstOption ===
+                    secondRecursedInterpreterState.options[optionIndex]
+                ))
+            ) {
+              stateAppearanceRecord.recursedInterpreterStates.splice(j, 1);
+
+              for (const stateAppearanceRecord of stateAppearanceRecords) {
+                for (const recursedInterpreterState of stateAppearanceRecord.recursedInterpreterStates) {
+                  switch (recursedInterpreterState.type) {
+                    case `dismiss`:
+                      if (
+                        recursedInterpreterState.next ===
+                        secondRecursedInterpreterState
+                      ) {
+                        recursedInterpreterState.next =
+                          firstRecursedInterpreterState;
+                      }
+                      break;
+
+                    case `menu`:
+                      for (
+                        let optionIndex = 0;
+                        optionIndex < recursedInterpreterState.options.length;
+                        optionIndex++
+                      ) {
+                        if (
+                          recursedInterpreterState.options[optionIndex] ===
+                          secondRecursedInterpreterState
+                        ) {
+                          recursedInterpreterState.options[optionIndex] =
+                            firstRecursedInterpreterState;
+                        }
+                      }
+                      break;
+                  }
+                }
+              }
+
+              thisIterationMadeChanges = true;
+            } else {
+              j++;
+            }
+          }
+        }
+      }
+    }
+    //#endregion
+
+    // #region Convert these states to the interchange format and return them.
+    const recursedInterpreterStatesInStateOrder =
+      stateAppearanceRecords.flatMap(
+        (stateAppearanceRecord) =>
+          stateAppearanceRecord.recursedInterpreterStates
+      );
+
+    const generateMapStateInteraction = (
+      stateAppearanceRecord: StateAppearanceRecord,
+      recursedInterpreterState: RecursedInterpreterState
+    ): MapStateInteraction => {
+      switch (recursedInterpreterState.type) {
+        case `dismiss`:
+          return {
+            type: `dismiss`,
+            stateIndex: recursedInterpreterStatesInStateOrder.indexOf(
+              recursedInterpreterState.next as RecursedInterpreterState
+            ),
+          };
+
+        case `menu`:
+          return {
+            type: `menu`,
+            options: (
+              stateAppearanceRecord.stateAppearance.options as ReadonlyArray<
+                ReadonlyArray<MapStateRun>
+              >
+            ).map((content, optionIndex) => ({
+              content,
+              stateIndex: recursedInterpreterStatesInStateOrder.indexOf(
+                recursedInterpreterState.options[
+                  optionIndex
+                ] as RecursedInterpreterState
+              ),
+            })),
+          };
+      }
+    };
+
+    return {
+      type: `valid`,
+      characters:
+        startingRecursedInterpreterState.interpreterState.characters.map(
+          (character) => ({
+            normalized: character.normalized,
+            verbatim: character.verbatim,
+          })
+        ),
+      states: stateAppearanceRecords.flatMap((stateAppearanceRecord) =>
+        stateAppearanceRecord.recursedInterpreterStates.map(
+          (recursedInterpreterState) => ({
+            characters: stateAppearanceRecord.stateAppearance.characters,
+            speakers: stateAppearanceRecord.stateAppearance.speakers,
+            background: stateAppearanceRecord.stateAppearance.background,
+            line: recursedInterpreterState.interpreterState.line,
+            interaction: generateMapStateInteraction(
+              stateAppearanceRecord,
+              recursedInterpreterState
+            ),
+          })
+        )
+      ),
+    };
+    // #endregion
+  }
+};
---
+++ unit.ts
@@ -0,0 +1,207 @@
+import type {
+  Document,
+  Map,
+  MapState,
+  MenuMapStateInteractionOption,
+} from "@skitscript/types-nodejs";
+import * as fs from "fs";
+import * as path from "path";
+import { map } from ".";
+import { parse } from "@skitscript/parser-nodejs";
+
+const casesPath = path.join(
+  __dirname,
+  `submodules`,
+  `skitscript`,
+  `mapper-test-suite`,
+  `cases`
+);
+
+const caseNames = fs.readdirSync(casesPath);
+
+for (const caseName of caseNames) {
+  describe(caseName, () => {
+    const casePath = path.join(casesPath, caseName);
+
+    let document: Document;
+    let actual: Map;
+
+    beforeAll(async () => {
+      const source = await fs.promises.readFile(
+        path.join(casePath, `input.skitscript`),
+        `utf8`
+      );
+
+      document = parse(source);
+
+      if (document.type === `valid`) {
+        actual = map(document);
+      }
+    });
+
+    it(`is parsable`, () => {
+      expect(document.type)
+        .withContext(JSON.stringify(document, null, 2))
+        .toEqual(`valid`);
+    });
+
+    it(`generates no warnings once parsed`, () => {
+      if (document.type === `valid`) {
+        expect(document.warnings)
+          .withContext(JSON.stringify(document.warnings, null, 2))
+          .toEqual([]);
+      }
+    });
+
+    it(`maps as expected`, async () => {
+      if (document.type === `valid`) {
+        const expectedText = await fs.promises.readFile(
+          path.join(casePath, `output.json`),
+          `utf8`
+        );
+
+        const expected: Map = JSON.parse(expectedText);
+
+        if (expected.type === `valid`) {
+          const expectedValid = expected;
+          expect(actual.type).toEqual(expectedValid.type);
+
+          if (actual.type === `valid`) {
+            const actualValid = actual;
+
+            expect(actualValid.characters).toEqual(expectedValid.characters);
+
+            expect(actualValid.states.length)
+              .withContext(JSON.stringify(actual, null, 2))
+              .toEqual(expectedValid.states.length);
+
+            if (actualValid.states.length === expectedValid.states.length) {
+              const actualStateIndicesByExpectedStateIndices: number[] = [];
+
+              const recurse = (
+                expectedStateIndex: number,
+                actualStateIndex: number,
+                path: ReadonlyArray<string>
+              ): void => {
+                const pathDescription = path.join(`, `);
+
+                if (
+                  Object.prototype.hasOwnProperty.call(
+                    actualStateIndicesByExpectedStateIndices,
+                    expectedStateIndex
+                  )
+                ) {
+                  expect(
+                    actualStateIndicesByExpectedStateIndices[expectedStateIndex]
+                  )
+                    .withContext(pathDescription)
+                    .toEqual(actualStateIndex);
+                } else {
+                  actualStateIndicesByExpectedStateIndices[expectedStateIndex] =
+                    actualStateIndex;
+
+                  expect(actualStateIndex)
+                    .withContext(pathDescription)
+                    .toBeLessThan(actualValid.states.length);
+
+                  if (actualStateIndex < actualValid.states.length) {
+                    const actualState = actualValid.states[
+                      actualStateIndex
+                    ] as MapState;
+                    const expectedState = expectedValid.states[
+                      expectedStateIndex
+                    ] as MapState;
+
+                    expect(actualState.characters)
+                      .withContext(pathDescription)
+                      .toEqual(expectedState.characters);
+
+                    expect(actualState.speakers)
+                      .withContext(pathDescription)
+                      .toEqual(expectedState.speakers);
+
+                    expect(actualState.background)
+                      .withContext(pathDescription)
+                      .toEqual(expectedState.background);
+
+                    expect(actualState.line)
+                      .withContext(pathDescription)
+                      .toEqual(expectedState.line);
+
+                    expect(expectedState.interaction.type)
+                      .withContext(pathDescription)
+                      .toEqual(actualState.interaction.type);
+
+                    const lineContent =
+                      expectedState.line === null
+                        ? ``
+                        : `"${expectedState.line
+                            .map((run) => run.plainText)
+                            .join(``)}"`;
+
+                    if (
+                      expectedState.interaction.type === `dismiss` &&
+                      actualState.interaction.type === `dismiss`
+                    ) {
+                      recurse(
+                        expectedState.interaction.stateIndex,
+                        actualState.interaction.stateIndex,
+                        [...path, lineContent]
+                      );
+                    } else if (
+                      expectedState.interaction.type === `menu` &&
+                      actualState.interaction.type === `menu`
+                    ) {
+                      expect(expectedState.interaction.options.length)
+                        .withContext(pathDescription)
+                        .toEqual(actualState.interaction.options.length);
+
+                      if (
+                        expectedState.interaction.options.length ===
+                        actualState.interaction.options.length
+                      ) {
+                        for (
+                          let optionIndex = 0;
+                          optionIndex <
+                          expectedState.interaction.options.length;
+                          optionIndex++
+                        ) {
+                          const expectedOption = expectedState.interaction
+                            .options[
+                            optionIndex
+                          ] as MenuMapStateInteractionOption;
+                          const actualOption = actualState.interaction.options[
+                            optionIndex
+                          ] as MenuMapStateInteractionOption;
+
+                          expect(expectedOption.content)
+                            .withContext(pathDescription)
+                            .toEqual(actualOption.content);
+
+                          recurse(
+                            expectedOption.stateIndex,
+                            actualOption.stateIndex,
+                            [
+                              ...path,
+                              `${lineContent} -> "${expectedOption.content
+                                .map((run) => run.plainText)
+                                .join(``)}"`,
+                            ]
+                          );
+                        }
+                      }
+                    }
+                  }
+                }
+              };
+
+              recurse(0, 0, []);
+            }
+          }
+        } else {
+          expect(actual).toEqual(expected);
+        }
+      }
+    });
+  });
+}
